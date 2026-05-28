75a4614bd553a91f7e21a15895e4c7b5d8bfa515
Volkov Ivan <steeltruedrum@gmail.com>
2026-05-28 16:21:22 +0300

    fixed 4. Create CarService to return List<Car>

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,27 +15,19 @@ import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
-    private final List<Car> carList = Arrays.asList(
-            new Car("VW", "Polo", 2021),
-            new Car("Audi", "A6", 2020),
-            new Car("Mercedes", "S-class", 2023),
-            new Car("Lada", "Kalina", 2014),
-            new Car("Lada", "2107", 2005)
-    );
+
+    private final CarService carService;
+
+    // Внедрение сервиса через конструктор
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
-        List<Car> resultList;
-
-        if (count == null || count >= 5) {
-            resultList = carList;
-        } else {
-            resultList = carList.stream()
-                    .limit(count)
-                    .collect(Collectors.toList());
-        }
-
-        model.addAttribute("cars", resultList);
+        // Контроллер делегирует задачу сервису
+        model.addAttribute("cars", carService.getCarsByCount(count));
         return "cars";
     }
 
---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,32 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarService {
+
+    // Список с машинами
+    private final List<Car> carList = Arrays.asList(
+            new Car("VW", "Polo", 2021),
+            new Car("Audi", "A6", 2020),
+            new Car("Mercedes", "S-class", 2023),
+            new Car("Lada", "Kalina", 2014),
+            new Car("Lada", "2107", 2005)
+    );
+
+    // Возвращаем список машин в зависимости от count
+    public List<Car> getCarsByCount(Integer count) {
+        if (count == null || count >= 5 || count < 0) {
+            return carList;
+        }
+        return carList.stream()
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+}
